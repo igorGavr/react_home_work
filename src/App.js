bd51e70259a68dcb144feca4c8be8519fb63a1bc
igorGavr <ragamuffinrogi@gmail.com>
2022-04-14 13:10:03 +0300

    less 4 react router dom v6

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-
+import {Routes, Route, Navigate} from "react-router-dom";
+import {MainLayout} from "./layouts";
+import {AboutPage, HomePage, NotFoundPage, PostsPage, UsersPage} from "./pages";
 
 
 function App() {
   return (
     <div className="App">
+      <Routes>
+        <Route path={'/'} element={<MainLayout/>}>
+          <Route index element={<Navigate to={'./home'}/>}/>
+          <Route path={'/home'} element={<HomePage/>}/>
+          <Route path={'/users'} element={<UsersPage/>}/>
+          <Route path={'/posts'} element={<PostsPage/>}/>
+          <Route path={'/about'} element={<AboutPage/>}/>
+          <Route path={'*'} element={<NotFoundPage/>}/>
+        </Route>
+      </Routes>
 
     </div>
   );
---
+++ src/layouts/MainLayout/MainLayout.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import {Outlet, Link, NavLink} from "react-router-dom"
+import css from './MainLayout.module.css'
+
+const MainLayout = () => {
+    return (
+        <div>
+            <div className={css.header}>
+                <NavLink to="home">Home</NavLink>
+                <NavLink to="users">Users</NavLink>
+                <NavLink to="posts">Posts</NavLink>
+                <NavLink to="about">About</NavLink>
+            </div>
+            <hr/>
+            <Outlet/>
+        </div>
+    );
+};
+
+export {MainLayout};
