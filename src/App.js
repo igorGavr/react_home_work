7d8a920b248aef89ae72a13abbc7b25c726ddaf0
igorGavr <ragamuffinrogi@gmail.com>
2022-04-23 14:57:19 +0300

    les 6 redux_base 1

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 
+import {useDispatch, useSelector} from "react-redux";
+
+const App = () => {
+  let state = useSelector(state => state);
+  let dispatch = useDispatch();
 
 
-function App() {
   return (
-    <div className="App">
+      <div>
+        <h1>state is - {state}</h1>
 
-    </div>
+        <button onClick={() => {
+          dispatch({type: 'INC', payload: 1});
+        }}>increment
+        </button>
+        <button onClick={() => {
+          dispatch({type: 'DEC', payload: 1});
+        }}>decrement
+        </button>
+        <button onClick={() => {
+          dispatch({type: 'RESET', payload: 0});
+
+        }}>reset
+        </button>
+
+      </div>
   );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,31 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
+import {Provider} from "react-redux";
+import {createStore} from 'redux';
 
 
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 
+function reducer(state = 0, action) {
+    switch (action.type) {
+        case 'INC':
+            return state + action.payload;
+        case 'DEC':
+            console.log(action.payload);
+            return state - action.payload;
+        case 'RESET':
+            console.log(action.payload);
+            state = action.payload;
+            return state;
+        default:
+            return state;
+    }
+}
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<App/>)
+let store = createStore(reducer);
+
+root.render(<Provider store={store}>
+    <App/>
+</Provider>)
