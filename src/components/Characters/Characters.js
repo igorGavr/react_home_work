af0db49ad01401b0783ae294c470ad1e237e923f
igorGavr <ragamuffinrogi@gmail.com>
2022-05-02 15:13:12 +0300

    rick and morty

--- src/components/Characters/Characters.js
+++ src/components/Characters/Characters.js
@@ -1,46 +1,26 @@
 import React, {useEffect, useState} from 'react';
-import {useLocation, useSearchParams} from "react-router-dom";
-import {useDispatch, useSelector} from "react-redux";
+import {useLocation} from "react-router-dom";
+
 
 import {characterService} from "../../services";
 import {Character} from "../Character/Character";
 import css from './Characters.module.css'
-import {characterActions} from "../../redux/slices/character.slice";
+
 
 const Characters = () => {
-    const [charactersByList, setCharactersByList] = useState([]);
+    const [characters, setCharacters] = useState([]);
     const {state} = useLocation();
-    const {query, setQuery} = useSearchParams({page: '1'});
-    const dispatch = useDispatch();
-    const {characters, next, prev} = useSelector(state => state.characters);
+    console.log(state)
 
     useEffect(() => {
-        if (state) {
-            characterService.getByCharacterList(state).then(({data}) => setCharactersByList(data))
-        }else {
-            dispatch(characterActions.getAll({page: query.get('page')}))
-        }
-    }, [state, query])
+        characterService.getByCharacterList(state).then(({data}) => setCharacters(data))
+    }, [])
 
-    const nextPage = () => {
-        const nextPage = +query.get('page') + 1;
-        setQuery({page: `${nextPage}`})
-    }
-    const prevPage = () => {
-        const prevPage = +query.get('page') - 1;
-        setQuery({page: `${prevPage}`})
-    }
     return (
         <div >
             <div className={css.characters}>
-                {charactersByList && charactersByList.map(item => <Character key={item.id} character={item}/>)}
-                {characters && characters.map(item => <Character key={item.id} character={item}/>)}
+                {characters.map(item => <Character key={item.id} character={item}/>)}
             </div>
-            <div>
-                <button disabled={!prev} onClick={prevPage}>Prev</button>
-                <button disabled={!next} onClick={nextPage}>Next</button>
-            </div>
-
         </div>
     );
 };
--- src/services/character.service.js
+++ src/services/character.service.js
@@ -1,9 +1,11 @@
 import {axiosService} from "./axios.service";
 import {urls} from "../constants";
+import {logDOM} from "@testing-library/react";
 
 const characterService = {
     getByCharacterList: (characters) => {
         const ids = characters.map(item => item.split('/').slice(-1)[0]).join(',')
+        console.log(ids)
         return axiosService.get(`${urls.character}/${ids}`)
     },
     getAllCharacters: (page = 1) => axiosService.get(urls.character, {params: {page}})
