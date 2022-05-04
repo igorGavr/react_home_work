00377c3d4f1be817fac9434ca92b2cab3cf57cf9
igorGavr <ragamuffinrogi@gmail.com>
2022-05-04 12:42:05 +0300

    Type Script

--- interfaces/user.interface.ts
+++ interfaces/user.interface.ts
@@ -1,4 +1,4 @@
-export interface IUser{
+export interface IUser {
     id: number
     username: string
     email: string
@@ -15,37 +15,72 @@ export interface IUser{
     }
 }
 
-export interface IExmp{
+interface IPayloads {
+    payload_type: string,
+    payload_mass: number,
+    payload_mass_lbs: number
+};
+
+interface ICores {
+    flight: number
+    core: {
+        reuse_count: number
+        status: string
+    }
+};
+
+export interface IExmp {
     mission_name: string
     launch_date_local: string
     launch_site: {
         site_name_long: string
     }
     links: {
-        article_link: null
+        article_link: string
         video_link: string
     }
     rocket: {
         rocket_name: string
         first_stage: {
-            cores: [
-                {
-                    flight: number
-                    core: {
-                        reuse_count: number
-                        status: string
-                    }
-                }
-            ]
+            cores: ICores[]
         },
         second_stage: {
-            payloads: [
-                {
-                    payload_type: string
-                    payload_mass: number
-                    payload_mass_lbs: number
-                }
-            ]
+            payloads: IPayloads[]
         }
     }
 }
+
+///////////////////////////////////////////////
+interface IUser2 {
+    name: string,
+    age: number,
+    gender: string
+}
+
+const user2: IUser2 = {
+    name: 'Max',
+    age: 18,
+    gender: 'male'
+}
+
+const sum = (a: number, b: number): number => a + b
+
+const incAge = (someUser: IUser2, inc: number): IUser2 => {
+    someUser.age += inc
+    return someUser
+}
+console.log(sum(1, 2))
+showSum(2, 3)
+incAge(user2, 2)
+
+/////////////////////////////////////////
+type Name = 'Jurik'|'Kutasik'|'Skirka'|'Zyat'
+
+interface IMokriy {
+    id?: number
+    name: Name
+}
+interface IMorkiyOne extends IMokriy{
+    age: number
+}
+const mokriy: Required<IMorkiyOne> = {name: "Jurik", age: 2, id: null}
